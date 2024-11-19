1590438c1903c783db1fd39a6a1d21a16c8006cc
JustSmoking <104763679+JustSmoking@users.noreply.github.com>
2024-11-19 01:46:09 +0100

    Add files via upload

---
+++ calc.h
@@ -0,0 +1,13 @@
+//
+// Created by JustSmoking on 18/11/2024.
+//
+
+#ifndef CALCULATRICE_POINTEUR_CALC_H
+#define CALCULATRICE_POINTEUR_CALC_H
+int charOperation(char *);
+int charOperationMultiplyOrder(char *);
+int charOperationDivideOrder(char *Operation);
+int charOperationByRespectOrder(char  *operation);
+
+
+#endif //CALCULATRICE_POINTEUR_CALC_H
